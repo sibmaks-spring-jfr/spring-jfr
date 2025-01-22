a5494cf2cb6649a8ca8bafc3da47b54d7592c1f6
sibmaks <sibmaks@vk.com>
2025-01-22 15:12:16 +0300

    Правка публикации зависимостей

--- app/src/main/java/io/github/sibmaks/spring/jfr/bean/JavaFlightRecorderMergedBeanDefinitionEventProducer.java
+++ app/src/main/java/io/github/sibmaks/spring/jfr/bean/JavaFlightRecorderMergedBeanDefinitionEventProducer.java
@@ -3,9 +3,16 @@ package io.github.sibmaks.spring.jfr.bean;
 import io.github.sibmaks.spring.jfr.core.ContextIdProvider;
 import io.github.sibmaks.spring.jfr.event.core.converter.DependencyConverter;
 import io.github.sibmaks.spring.jfr.event.publish.bean.MergedBeanDefinitionRegisteredEvent;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.MergedBeanDefinitionPostProcessor;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author sibmaks
@@ -29,6 +36,26 @@ public final class JavaFlightRecorderMergedBeanDefinitionEventProducer implement
             Class<?> beanType,
             String beanName
     ) {
+        produce(beanDefinition, beanName, beanType);
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        var beanType = bean.getClass();
+        if (BeanFactoryUtils.isGeneratedBeanName(beanName)) {
+            produceGenerated(beanName, beanType);
+            return bean;
+        }
+        if (!beanFactory.containsBeanDefinition(beanName)) {
+            produceGenerated(beanName, beanType);
+            return bean;
+        }
+        var beanDefinition = beanFactory.getMergedBeanDefinition(beanName);
+        produce(beanDefinition, beanName, beanType);
+        return bean;
+    }
+
+    private void produce(BeanDefinition beanDefinition, String beanName, Class<?> beanType) {
         var beanClass = BeanDefinitions.getBeanType(beanDefinition, beanType);
         var beanClassName = beanClass.getCanonicalName();
         var stereotype = BeanDefinitions.getStereotype(beanClass);
@@ -45,6 +72,25 @@ public final class JavaFlightRecorderMergedBeanDefinitionEventProducer implement
                 .dependencies(DependencyConverter.convert(dependencies.toArray(String[]::new)))
                 .stereotype(stereotype.name())
                 .generated(false)
+                .build()
+                .commit();
+    }
+
+    private void produceGenerated(String beanName, Class<?> beanType) {
+        var dependencies = Optional.of(beanFactory.getDependenciesForBean(beanName))
+                .map(List::of)
+                .map(HashSet::new)
+                .orElseGet(HashSet::new);
+
+        var contextId = contextIdProvider.getContextId();
+        var beanClassName = beanType.getCanonicalName();
+
+        MergedBeanDefinitionRegisteredEvent.builder()
+                .contextId(contextId)
+                .beanClassName(beanClassName)
+                .beanName(beanName)
+                .dependencies(DependencyConverter.convert(dependencies.toArray(String[]::new)))
+                .generated(true)
                 .build()
                 .commit();
     }
