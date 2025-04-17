7230916577fce3a120649cf96122469794779bde
sibmaks <sibmaks@vk.com>
2025-04-17 10:03:41 +0300

    Aspect only components by component advice

--- app/src/main/java/io/github/sibmaks/spring/jfr/tracing/component/ComponentJavaFlightRecorderBeanPostProcessor.java
+++ app/src/main/java/io/github/sibmaks/spring/jfr/tracing/component/ComponentJavaFlightRecorderBeanPostProcessor.java
@@ -3,8 +3,13 @@ package io.github.sibmaks.spring.jfr.tracing.component;
 import io.github.sibmaks.spring.jfr.JavaFlightRecorderRecordCounter;
 import io.github.sibmaks.spring.jfr.tracing.GenericAspectBeanPostProcessor;
 import org.aopalliance.aop.Advice;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -35,6 +40,11 @@ public class ComponentJavaFlightRecorderBeanPostProcessor extends GenericAspectB
 
     @Override
     protected boolean isAspectBean(Object bean, Class<?> type, String beanName) {
-        return AnnotatedElementUtils.hasAnnotation(type, Component.class);
+        return AnnotatedElementUtils.hasAnnotation(type, Component.class) &&
+                !AnnotatedElementUtils.hasAnnotation(type, Controller.class) &&
+                !AnnotatedElementUtils.hasAnnotation(type, RestController.class) &&
+                !AnnotatedElementUtils.hasAnnotation(type, Service.class) &&
+                !AnnotatedElementUtils.hasAnnotation(type, Repository.class) &&
+                !AnnotatedElementUtils.hasAnnotation(type, Configuration.class);
     }
 }
